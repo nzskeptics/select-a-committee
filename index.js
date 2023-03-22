32d74694819bd0731aa262130384fbc9139044a0
Mark Honeychurch <mark@honeychurch.org>
2023-03-23 02:46:16 +1300

    New packages

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
-// https://bills.parliament.nz/api/data/v1/search
-
-// { 	"documentPreset": 11, 	"keyword": null, 	"selectCommittee": null, 	"itemType": null, 	"itemSubType": null, 	"status": [], 	"documentTypes": [], 	"beforeCommittee": null, 	"billStage": null, 	"billStages": [], 	"billTab": null, 	"billId": "e52e16e3-0427-4372-a650-4eb655b4c440", 	"includeBillStages": null, 	"subject": null, 	"person": null, 	"parliament": null, 	"dateFrom": null, 	"dateTo": null, 	"datePeriod": null, 	"restrictedFrom": null, 	"restrictedTo": null, 	"terminatedReason": null, 	"terminatedReasons": [], 	"column": 4, 	"direction": 1, 	"pageSize": {{ $item(0).$node["Set page number"].json["records"] }}, 	"page": {{ $item(0).$node["Set page number"].json["page"] + $runIndex }} }
+import rake from 'rake-js';
+import {reporter} from 'vfile-reporter';
+import {toString} from 'nlcst-to-string';
+import {unified} from 'unified';
+import retextEnglish from 'retext-english';
+import retextEquality from 'retext-equality';
+import retextIndefiniteArticle from 'retext-indefinite-article';
+import retextContractions from 'retext-contractions';
+import retextDiacritics from 'retext-diacritics';
+import retextStringify from 'retext-stringify';
+import retextPos from 'retext-pos';
+import retextKeywords from 'retext-keywords';
+import retextReadability from 'retext-readability';
+import retextRepeatedWords from 'retext-repeated-words';
+import retextSentenceSpacing from 'retext-sentence-spacing';
+import retextRedundantAcronyms from 'retext-redundant-acronyms';
+import retextSpell from 'retext-spell';
+import retextOveruse from 'retext-overuse';
+import retextUsage from 'retext-usage';
+import dictionary from 'dictionary-en-gb';
+import {removeStopwords} from 'stopword';
 
 const url = 'https://bills.parliament.nz/api/data/v1/search';
 const fileurl = 'https://www.parliament.nz';
