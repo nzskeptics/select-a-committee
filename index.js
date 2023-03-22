3dfe4572239e64ec0db47d4aeb794dc5d2f6c30f
Mark Honeychurch <mark.honeychurch@telesmart.co.nz>
2023-03-23 11:49:30 +1300

    Caching is now working

--- index.js
+++ index.js
@@ -51,10 +51,10 @@ async function getSubmissions(url) {
 	return data.results;
 }
 
-async function getSubmissionHTML(url, path) {
-	const path = `cache/${id}.html`;
-	if (existsSync(path)) return readFileSync(path);
-	const response = await fetch(url + path);
+async function getSubmissionHTML(url, name, folder = 'html') {
+	const path = `cache/${folder}/${name}.html`;
+	if (existsSync(path)) return readFileSync(path, 'utf8');
+	const response = await fetch(url);
 	if (!response.ok) {
 		console.error(`HTTP error! status: ${response.status}`);
 		console.error(response);
@@ -66,46 +66,46 @@ async function getSubmissionHTML(url, path) {
 }
 
 async function getSubmissionPDF(url, id) {
-	const path = `cache/${id}.pdf`;
+	const path = `cache/pdf/${id}.pdf`;
 	if (existsSync(path)) return readFileSync(path);
-	const response = await fetch(url + id);
+	const response = await fetch(url);
 	if (!response.ok) {
 		console.error(`HTTP error! status: ${response.status}`);
 		console.error(response);
 		return;
 	}
-	const data = await response.text();
-	writeFileSync(path, data);
+	const data = await response.arrayBuffer();
+	writeFileSync(path, Buffer.from(data));
 	return data;
 }
 
 async function processSubmission(text) {
 	const processor = unified()
-	.use(retextEnglish)
-	.use(retextEquality)
-	.use(retextIndefiniteArticle)
-	.use(retextContractions)
-	.use(retextDiacritics)
-	.use(retextStringify)
-	.use(retextPos)
-	.use(retextKeywords)
-	.use(retextReadability)
-	.use(retextRepeatedWords)
-	.use(retextSentenceSpacing)
-	.use(retextRedundantAcronyms)
-	.use(retextSpell, dictionary)
-	.use(retextOveruse)
-	.use(retextUsage);
-const file = await processor.process(text);
-console.log(reporter(file));
-console.log(file.data.keywords);
-console.log(file.data.overusedWords);
-console.log(file.data.repeatedWords);
-console.log(file.data.redundantAcronyms);
-console.log(file.data.sentenceSpacing);
-console.log(file.data.readability);
-console.log(file.data.spellingSuggestions);
-console.log(file.data.suggestions);
+		.use(retextEnglish)
+		.use(retextEquality)
+		.use(retextIndefiniteArticle)
+		.use(retextContractions)
+		.use(retextDiacritics)
+		.use(retextStringify)
+		.use(retextPos)
+		.use(retextKeywords)
+		.use(retextReadability)
+		.use(retextRepeatedWords)
+		.use(retextSentenceSpacing)
+		.use(retextRedundantAcronyms)
+		.use(retextSpell, dictionary)
+		.use(retextOveruse)
+		.use(retextUsage);
+	const file = await processor.process(text);
+	console.log(reporter(file));
+	console.log(file.data.keywords);
+	console.log(file.data.overusedWords);
+	console.log(file.data.repeatedWords);
+	console.log(file.data.redundantAcronyms);
+	console.log(file.data.sentenceSpacing);
+	console.log(file.data.readability);
+	console.log(file.data.spellingSuggestions);
+	console.log(file.data.suggestions);
 }
 
 async function getAll() {
@@ -116,15 +116,19 @@ async function getAll() {
 	};
 	const submissions = await getSubmissions(urls.search);
 	for (const submission of submissions.sort((a, b) => a.title.localeCompare(b.title))) {
+		console.info(submission.title);
 		submission.urls = {
 			page: urls.page + submission.url,
 			metadata: urls.page + '/en/document/' + submission.id + '/metadata',
 			pdf: urls.pdf + submission.id,
 		};
-		submission.html = await getSubmissionHTML(submission.page);
-		submission.metadata = await getSubmissionHTML(submission.metapage);
-		submission.pdf = await getSubmission(urls.pdf, submission.id);
-		console.info(submission.title);
+		submission.metadata = await getSubmissionHTML(submission.urls.metadata, submission.id, 'metadata');
+		submission.html = await getSubmissionHTML(submission.urls.page, submission.id);
+		// Use regex to find the key in an HTML link that looks like <a href="/resource/en-NZ/53SCHE_EVI_130084_HE43428/7f637b73a4d81b311744d8b129433b421ddd1606">, where the key is "7f637b73a4d81b311744d8b129433b421ddd1606"
+		// submission.key = submission.html.match(/\/resource\/en-NZ\/[a-zA-Z0-9_]+\/[a-zA-Z0-9_]+\//)[0];
+		submission.key = submission.html.match(/\/resource\/en-NZ\/[A-Z0-9_]+\/([a-z0-9]+)"/)[1];
+		submission.urls.pdf += '/' + submission.key;
+		submission.pdf = await getSubmissionPDF(submission.urls.pdf, submission.id);
 	}
 }
 
